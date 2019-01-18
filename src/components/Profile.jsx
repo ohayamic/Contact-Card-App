34a038e895635baadf464d63fb89b8abeb371e68
ohaya <ohayamic@gmail.com>
2019-01-19 00:55:35 +0100

    Added a new profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
+import { Grid, Col, Row, Image } from "react-bootstrap";
 class Profile extends Component {
   state = {};
   render() {
-    return <div>Profile</div>;
+    return (
+      <div>
+        <Grid bsClass="container">
+          <Row>
+            <Col xs={12} sm={9} style={{ paddingRight: 20 }}>
+              <Image
+                src="assets/MichaelOnyebuchiOhaya_1.jpg"
+                style={{ width: "100%" }}
+                responsive
+              />
+            </Col>
+
+            <Col xsHidden sm={3}>
+              side bar
+            </Col>
+          </Row>
+        </Grid>
+
+        <Grid>
+          <Col xs={12} sm={9}>
+            <Image
+              src="assets/MichaelOnyebuchiOhaya_2.jpg"
+              style={{ width: "100%" }}
+              responsive
+            />
+          </Col>
+          <Col xsHidden sm={3}>
+            side bar
+          </Col>
+        </Grid>
+        <Grid>
+          <Col xs={12} sm={9}>
+            <Image
+              src="assets/MichaelOnyebuchiOhaya_3.jpg"
+              style={{ width: "100%" }}
+              responsive
+            />
+          </Col>
+          <Col xsHidden sm={3}>
+            side bar
+          </Col>
+        </Grid>
+      </div>
+    );
   }
 }
 
